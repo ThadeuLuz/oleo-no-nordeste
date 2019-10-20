b20b6530448a8d6e41b60f07c6e6910800558479
Thadeu Luz <thadeuluz@gmail.com>
2019-10-20 02:38:20 -0300

    Tarefa para converter csv p json

---
+++ tools/csvToJson.js
@@ -0,0 +1,17 @@
+const parse = require('csv-parse')
+const fs = require('fs-extra')
+
+const input = fs.readFileSync('./tools/2019-10-19_LOCALIDADES_AFETADAS_PLANILHA.csv', 'utf-8')
+
+const options = {
+  delimiter: ';',
+  columns: true,
+}
+
+parse(input, options, function(err, output){
+  if (err) {
+    return console.error(err)
+  }
+
+  fs.writeJsonSync('./src/data/praias.json', output, { spaces: 2 })
+})
