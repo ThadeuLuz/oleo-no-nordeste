175df6b2fa03b93bf5df5cc93320290915920699
Thadeu Luz <thadeuluz@gmail.com>
2019-10-20 12:46:51 -0300

    Hello World rebass

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+)
+
+export default App;
