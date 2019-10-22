980302c98f0605253a3f1e7032b98e3757875c4f
Thadeu Luz <thadeuluz@gmail.com>
2019-10-22 12:11:44 -0300

    Melhorar pipeline de dados

--- tools/csvToJson.js
+++ tools/csvToJson.js
@@ -1,7 +1,11 @@
 const parse = require('csv-parse')
 const fs = require('fs-extra')
+const { promisify } = require('util')
+// const { mapsApiKey } = require('../config')
 
-// Esta tabela foi exportada a partir do XLS disponível no site oficial do Ibama:
+const asyncParse = promisify(parse)
+
+// Este CSV foi exportado a partir do XLS disponível no site oficial do Ibama:
 // https://www.ibama.gov.br/notas/2047-manchas-de-oleo-no-litoral-do-nordeste
 const input = fs.readFileSync(
   './tools/2019-10-19_LOCALIDADES_AFETADAS_PLANILHA.csv',
@@ -13,35 +17,69 @@ var options = {
   // columns: ,
 }
 
-// O nome das colunas é bizonho, vamos renomear.
-const columns = [
-  'nome',
-  'municipio',
-  'dataAvist',
-  'estado',
-  'lat',
-  'long',
-  'dataRevis',
-  'status',
-]
+/**
+ * Converte a geolocalização do formato DMS (Graus, minutos e segundos)
+ * Tipo  ["5° 35' 10.09\" S", "35° 13' 38.60\" W"]
+ * para [-5.586136111111111, -35.22738888888889 ]
+ * A api do google maps exige parâmetros em DD.
+ * @see https://www.gps-coordinates.net/gps-coordinates-converter
+ * @see https://developers.google.com/maps/documentation/maps-static/dev-guide
+ */
+const dmsToDd = str => {
+  const [d, m, s] = str
+    .replace("'", '')
+    .replace('"', '')
+    .replace('°', '')
+    // Como todos os pontos são S e W, pode tirar e multiplicar por -1 no fim
+    .replace('S', '')
+    .replace('W', '')
+    .split(' ')
+    .filter(v => !!v)
+    .map(s => parseFloat(s))
 
-parse(input, options, function(err, output) {
-  if (err) {
-    return console.error(err)
-  }
+  return -(d + m / 60 + s / 3600)
+}
 
-  const finalData = output
-    .map(values => {
-      const location = {}
-      columns.forEach((key, index) => {
-        location[key] =
-          key === 'dataAvist' || key === 'dataRevis'
-            ? new Date(values[index])
-            : values[index]
-      })
-      return location
-    })
-    .filter(l => l && l.nome !== 'Name')
-
-  fs.writeJsonSync('./src/data/localidades.json', finalData, { spaces: 2 })
+/**
+ * Converte um data point de uma lista de arrays em um objeto que será salvo no
+ * .json
+ */
+const cleanDataPoint = ([
+  nome,
+  municipio,
+  dataAvist,
+  estado,
+  lat,
+  long,
+  dataRevis,
+  status,
+]) => ({
+  nome,
+  municipio,
+  estado,
+  status,
+  // Colunas que convertemos em data
+  dataRevis: new Date(dataRevis),
+  dataAvist: new Date(dataAvist),
+  // Colunas com geoloc
+  lat: dmsToDd(lat),
+  long: dmsToDd(long),
 })
+
+const run = async () => {
+  // Resultado do csv
+  const output = await asyncParse(input, options)
+
+  // Remove o primeiro item (nome das colunas)
+  output.shift()
+
+  // Limpa os dados
+  const cleanData = output.map(cleanDataPoint)
+
+  // Escreve o json em disco
+  fs.writeJsonSync('./src/data/localidades.json', cleanData, { spaces: 2 })
+
+  console.log(cleanData[0])
+}
+
+run()
