fd45893da376f1fb5f04e84747d706dabab37b14
Thadeu Luz <thadeuluz@gmail.com>
2019-10-22 12:14:01 -0300

    Página inicial

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../../components/Layout'
+import LocalidadeList from '../../components/LocalidadeList'
+
+const LocalidadesPage = () => (
+  <Layout title="Localidades">
+    <LocalidadeList />
+  </Layout>
+)
+
+export default LocalidadesPage
