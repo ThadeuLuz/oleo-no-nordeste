0a176e6c94e701b1e428fe28b152b1eb2fe714e4
Thadeu Luz <thadeuluz@gmail.com>
2019-10-22 00:56:16 -0300

    Melhorar export de dataset

--- tools/csvToJson.js
+++ tools/csvToJson.js
@@ -1,6 +1,8 @@
 const parse = require('csv-parse')
 const fs = require('fs-extra')
 
+// Esta tabela foi exportada a partir do XLS disponível no site oficial do Ibama:
+// https://www.ibama.gov.br/notas/2047-manchas-de-oleo-no-litoral-do-nordeste
 const input = fs.readFileSync(
   './tools/2019-10-19_LOCALIDADES_AFETADAS_PLANILHA.csv',
   'utf-8',
@@ -8,13 +10,38 @@ const input = fs.readFileSync(
 
 var options = {
   delimiter: ';',
-  columns: true,
+  // columns: ,
 }
+
+// O nome das colunas é bizonho, vamos renomear.
+const columns = [
+  'nome',
+  'municipio',
+  'dataAvist',
+  'estado',
+  'lat',
+  'long',
+  'dataRevis',
+  'status',
+]
 
 parse(input, options, function(err, output) {
   if (err) {
     return console.error(err)
   }
 
-  fs.writeJsonSync('./src/data/praias.json', output, { spaces: 2 })
+  const finalData = output
+    .map(values => {
+      const location = {}
+      columns.forEach((key, index) => {
+        location[key] =
+          key === 'dataAvist' || key === 'dataRevis'
+            ? new Date(values[index])
+            : values[index]
+      })
+      return location
+    })
+    .filter(l => l && l.nome !== 'Name')
+
+  fs.writeJsonSync('./src/data/localidades.json', finalData, { spaces: 2 })
 })
