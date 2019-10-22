08fc1236d9b6d38363fb52cf63cd655291406080
Thadeu Luz <thadeuluz@gmail.com>
2019-10-22 12:13:52 -0300

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Box, Text, Link } from 'rebass'
+
+const Footer = () => (
+  <Box bg="black">
+    <Box
+      p={4}
+      sx={{
+        maxWidth: 600,
+        mx: 'auto',
+        px: 3,
+      }}
+    >
+      <Text textAlign="center" color="white">
+        Sugestões para os próximos passos são MUITO bem vindas. Por favor, use o
+        issues do{' '}
+        <Link href="https://github.com/ThadeuLuz/oleo-nordeste" target="_blank">
+          GitHub
+        </Link>{' '}
+        (mais opções em breve).
+      </Text>
+      <Text color="white" textAlign="center">
+        Feito em Maceió 🏝
+      </Text>
+    </Box>
+  </Box>
+)
+
+export default Footer
