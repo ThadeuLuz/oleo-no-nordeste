379eb44ac842ae7cab3467c3d69bf4324d8a9f27
Raphael Amorim <rapha850@gmail.com>
2019-10-27 18:24:47 -0300

    adicionar nvmrc, editorconfig e prevenir o flick das divs/imagens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box, Text, Link } from 'rebass'
 
 const Footer = () => (
-  <Box bg="black">
+  <Box bg='black'>
     <Box
       p={4}
       sx={{
@@ -11,15 +11,15 @@ const Footer = () => (
         px: 3,
       }}
     >
-      <Text textAlign="center" color="white">
+      <Text textAlign='center' color='white'>
         Sugestões para os próximos passos são MUITO bem vindas. Por favor, use o
         issues do{' '}
-        <Link href="https://github.com/ThadeuLuz/oleo-nordeste" target="_blank">
+        <Link href='https://github.com/ThadeuLuz/oleo-nordeste' target='_blank'>
           GitHub
         </Link>{' '}
         (mais opções em breve).
       </Text>
-      <Text color="white" textAlign="center">
+      <Text color='white' textAlign='center'>
         Feito em Maceió 🏝
       </Text>
     </Box>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,17 @@ import { Flex, Box, Link, Heading, Image } from 'rebass'
 import github from '../images/github.svg'
 
 const Navbar = () => (
-  <Flex px={2} py={1} width={1} alignItems="center" bg="black">
-    <Heading p={2} fontWeight="bold" color="white">
+  <Flex px={2} py={1} width={1} alignItems='center' bg='black'>
+    <Heading p={2} fontWeight='bold' color='white'>
       Óleo no Nordeste
     </Heading>
-    <Box mx="auto" sx={{ boxShadow: 'card' }} />
+    <Box mx='auto' sx={{ boxShadow: 'card' }} />
     <Link
-      color="white"
-      href="https://github.com/ThadeuLuz/oleo-nordeste"
-      target="_blank"
+      color='white'
+      href='https://github.com/ThadeuLuz/oleo-nordeste'
+      target='_blank'
     >
-      <Image src={github} variant="icon" />
+      <Image src={github} variant='icon' />
     </Link>
   </Flex>
 )
